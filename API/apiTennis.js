99b2c21a398b16de2fa7c3160a4ad5e5223814a6
jbouteille <57391873+jbouteille@users.noreply.github.com>
2020-04-04 19:07:06 +0200

    1

--- API/apiTennis.js
+++ API/apiTennis.js
@@ -1,7 +1,19 @@
-var express = require('express');
-var mysql = require('mysql');
-var sql = require('mssql');
-var date = require('./date');
+const express = require('express');
+const mysql = require('mysql');
+const date = require('./date/date');
+const app =express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const requete = require('request');
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('./node_modules/yamljs');
+const swaggerDocument = YAML.load('./swagger/swagger.yaml');
+
+app.use(cors());
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(express.static('swagger'));
+app.use('/api/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 var pool = mysql.createPool({ //paramètres de connexion mysql
     connectionLimit: 10,
@@ -14,12 +26,12 @@ var pool = mysql.createPool({ //paramètres de connexion mysql
 
 var server = 'localhost';
 var port = 3000;
-var app = express();
+
 
 //route
 var myRouter = express.Router();
 
-myRouter.route('/joueurs/all')
+myRouter.route('/joueurs')
     .get(function(req, res){ //connexion mysql
         pool.getConnection(function(err, connection) {
             if (err) throw err;
@@ -36,7 +48,9 @@ myRouter.route('/joueurs/all')
     })
 
 myRouter.route('/joueur')
-    .get(function(req, res){ //connexion mysql
+    .get(function(req, res){ 
+        
+        //connexion mysql
         pool.getConnection(function(err, connection) {
             if (err) throw err;
         
@@ -51,19 +65,86 @@ myRouter.route('/joueur')
         });
     })
 
+//prochains matchs 
 myRouter.route('/matchs/prochain')
     .get(function(req,res){
         dateJour = date.getDate();
         pool.getConnection(function(err, connection){
             if(err) throw err;
 
-            connection.query("SELECT * FROM rencontres where date > '" + dateJour + "'", function(err, result, fields){
+            connection.query("SELECT Joueur1.Nom AS J1, Joueur2.Nom AS J2, DATE_FORMAT(date, '%d/%m/%y') as Date, lieu, court FROM rencontres INNER JOIN joueurs AS Joueur1 ON rencontres.idJoueur1 = Joueur1.id INNER JOIN joueurs AS Joueur2 ON rencontres.idJoueur2 = Joueur2.id WHERE Date >= CURRENT_DATE ORDER BY date DESC LIMIT 0," + req.query.nbMatch , function(err, result, fields){
                 connection.release();
                 if (err) throw err;
-                console.log(dateJour);
+                var i = 0;
+                while(result[i] != null)
+                {
+                    var test = apiMeteo(result[i].lieu);
+                    console.log(test);
+                    i++;
+                }
                 res.send(JSON.stringify(result));
             });
         });
+    })
+
+//Resultats 5 derniers matchs du joueur
+myRouter.route('/resultats')
+    .post(function(req,res){
+
+        nomJoueur = req.body.name;
+        if(nomJoueur)
+        {
+            pool.getConnection(function(err, connection){
+                if(err) throw err;
+
+                connection.query("SELECT Joueur1.Nom AS J1, Joueur2.Nom AS J2, JoueurVainqueur.Nom AS Vainqueur, date, lieu, court, set1, set2, set3, set4, set5 FROM rencontres INNER JOIN joueurs AS Joueur1 ON rencontres.idJoueur1 = Joueur1.id INNER JOIN joueurs AS Joueur2 ON rencontres.idJoueur2 = Joueur2.id INNER JOIN resultat ON rencontres.id = resultat.idMatch INNER JOIN joueurs AS JoueurVainqueur ON resultat.idVainqueur = JoueurVainqueur.id WHERE Joueur1.Nom = '" + nomJoueur + "' OR Joueur2.Nom = '" + nomJoueur + "' ORDER BY date DESC LIMIT 0,5", function(err, result, fields){
+                    connection.release();
+                    if (err) throw err;
+                    res.send(JSON.stringify(result));
+                });
+            });
+        }
+        else
+        {
+            res.status(400).send("Il manque le nom du joueur");
+        }
+
+    })
+
+//Statistiques des joueurs
+myRouter.route('/stats')
+    .post(function(req,res){
+
+        nomJoueur = req.body.name;
+        nbMatch = req.body.nbMatch;
+        if(nomJoueur)
+        {
+            if(nbMatch == null) nbMatch = 10;
+            pool.getConnection(function(err, connection){
+                if(err) throw err;
+
+                connection.query("SELECT Joueur1.Nom AS J1, Joueur2.Nom AS J2, JoueurVainqueur.Nom AS Vainqueur, date, lieu, court, set1, set2, set3, set4, set5 FROM rencontres INNER JOIN joueurs AS Joueur1 ON rencontres.idJoueur1 = Joueur1.id INNER JOIN joueurs AS Joueur2 ON rencontres.idJoueur2 = Joueur2.id INNER JOIN resultat ON rencontres.id = resultat.idMatch INNER JOIN joueurs AS JoueurVainqueur ON resultat.idVainqueur = JoueurVainqueur.id WHERE Joueur1.Nom = '" + nomJoueur + "' OR Joueur2.Nom = '" + nomJoueur + "' ORDER BY date DESC LIMIT 0," + nbMatch, function(err, result, fields){
+                    connection.release();
+                    if (err) throw err;
+                    i = 0;
+                    nbVictoire = 0;
+                    while(result[i] != null)
+                    {
+                        if(result[i].Vainqueur == nomJoueur) nbVictoire++;
+                        i++;
+                    }
+
+                    statMatchs = nbMatch/nbVictoire * 100;
+                    var tab = { "name" : nomJoueur, "statsVictoire:" : statMatchs + "%"};
+                    res.send(JSON.stringify(tab));
+                });
+            });
+        }
+        else
+        {
+            res.status(400).send("Il manque le nom du joueur");
+        }
+
     })
 
 
@@ -72,3 +153,20 @@ app.use(myRouter);
 app.listen(port, server, function() {
     console.log("Serveur : %s:%s", server, port);
 });
+
+function apiMeteo(lieu){
+
+    var url = "https://api-adresse.data.gouv.fr/search/?q="+lieu;
+    const insee = 0;
+    requete(url, function(error,response,body){
+        if (!error && response.statusCode == 200){
+            test = JSON.parse(body);
+
+            insee = test.features[0].properties.id;
+        }
+    });
+
+    return insee;
+
+
+}
---
+++ API/date/ville.js
@@ -0,0 +1,3 @@
+exports.getInforVille = function(){
+    
+}
